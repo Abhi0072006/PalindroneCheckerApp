6289bd3a28aa85fa86a1419292991549def5e401
Abhi0072006 <abhirudhsreesajan@gmail.com>
2026-02-27 11:02:07 +0530

    ABHIRUDH  Added UC4 CharArray Palindrome Check

---
+++ App/src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to check: ");
+        String input = scanner.nextLine();
+
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+
+        scanner.close();
+    }
+}
